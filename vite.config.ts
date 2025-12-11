bfd00080725ec0fb1bd82ae8aeff9b7da82537a3
meme2046 <meme2046@outlook.com>
2025-12-11 18:54:47 +0800

    project init

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,8 @@
-import path from "path";
+import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		react({
-			babel: {
-				plugins: [["babel-plugin-react-compiler"]],
-			},
-		}),
-		tailwindcss(),
-	],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
