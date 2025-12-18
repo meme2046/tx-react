41edfe63cc58a5416ee3e751ddc4f6ebefa4f940
meme2046 <meme2046@outlook.com>
2025-12-18 10:55:29 +0800

    _

--- src/routes/_layout.bitget-sf.tsx
+++ src/routes/_layout.bitget-sf.tsx
@@ -2,6 +2,13 @@ import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_layout/bitget-sf")({
 	component: RouteComponent,
+	head: () => ({
+		meta: [
+			{
+				title: "Bitget SF",
+			},
+		],
+	}),
 });
 
 function RouteComponent() {
