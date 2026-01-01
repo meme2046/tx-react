2ffb74640adb636f3cc9561c305ef35318d95402
meme2046 <meme2046@outlook.com>
2026-01-01 14:06:08 +0800

    _

--- src/hooks/gushitong/use-gushitong.ts
+++ src/hooks/gushitong/use-gushitong.ts
@@ -1,53 +1,6 @@
-import type {
-  BannerItem,
-  BannerResult,
-  Gushitong,
-  OpenData,
-} from "@/types/Gushitong";
+import type { Gushitong } from "@/types/Gushitong";
 import { http } from "@/utils";
 import { useQuery } from "@tanstack/react-query";
-
-// export const useBanner = (market: string) => {
-//   return useQuery({
-//     queryKey: ["gushitong-banner"],
-//     queryFn: () => {
-//       return http<Gushitong<BannerResult>>(
-//         "https://meme.us.kg:8888/proxy/finance.pae.baidu.com/api/getbanner",
-//         {
-//           params: { market: market },
-//         },
-//       ).then((d) => {
-//         if (d.ResultCode !== "0" && d.ResultCode !== 0) {
-//           throw new Error(`ResultCode:${d.ResultCode}, is not success`);
-//         }
-//         return d;
-//       });
-//     },
-//     refetchOnWindowFocus: false,
-//     refetchInterval: 70 * 1000,
-//     refetchIntervalInBackground: false,
-//   });
-// };
-
-// export const useIndexBanner = () => {
-//   return useQuery({
-//     queryKey: ["gushitong-indexbanner"],
-//     queryFn: () => {
-//       return http<Gushitong<BannerItem[]>>(
-//         "https://meme.us.kg:8888/proxy/finance.pae.baidu.com/api/indexbanner",
-//       ).then((d) => {
-//         if (d.ResultCode !== "0" && d.ResultCode !== 0) {
-//           throw new Error(`ResultCode:${d.ResultCode}, is not success`);
-//         }
-//         return d;
-//       });
-//     },
-//     refetchOnWindowFocus: false,
-//     refetchInterval: 60 * 1000,
-//     refetchIntervalInBackground: false,
-//   });
-// };
-
 export const useRedis = <T>(key: string) => {
   return useQuery({
     queryKey: [`gushitong-opendata-${key}`],
@@ -59,21 +12,6 @@ export const useRedis = <T>(key: string) => {
           throw new Error(`ERR: ${d.error}`);
         }
 
-        // if (key.startsWith("baidu.gushitong.opendata")) {
-        //   const result = d.data?.data[0]?.DisplayData.resultData.tplData.result;
-        //   const data: OpenData = {
-        //     name: result.name,
-        //     code: result.code,
-        //     marketName: result.marketName,
-        //     tag_list: result.tag_list,
-        //     minute_data: {
-        //       cur: result.minute_data.cur,
-        //       basicinfos: result.minute_data.basicinfos,
-        //     },
-        //     finance_type: result.finance_type,
-        //   };
-        //   return data;
-        // }
         return d.data;
       });
     },
