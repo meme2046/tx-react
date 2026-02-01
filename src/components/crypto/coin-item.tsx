e2a341f7da9addc9a559a79cd7a96b846c58aa6b
meme2046 <meme2046@outlook.com>
2026-02-01 13:27:31 +0800

    refactor(components): 使用七牛云获取交易所图标资源
    替换硬编码的SVG_SRC为通过getQiniuSrc动态获取图标资源

--- src/components/crypto/coin-item.tsx
+++ src/components/crypto/coin-item.tsx
@@ -1,42 +1,43 @@
-import { COIN_SRC, SVG_SRC } from "@/consts";
+import { COIN_SRC } from "@/consts";
 import { Button } from "../ui/button";
 import { ReactSVG } from "react-svg";
+import { getQiniuSrc } from "@/utils/qiniu";
 
 export function CoinItem({
-	coin,
-	cex,
-	dex,
+  coin,
+  cex,
+  dex,
 }: {
-	coin: string;
-	cex: string;
-	dex?: string;
+  coin: string;
+  cex: string;
+  dex?: string;
 }) {
-	return (
-		<div className="flex flex-col gap-1 items-start">
-			<Button variant="ghost" size="sm" className="p-1">
-				<img className="mr-2 w-6" src={COIN_SRC[coin.toLowerCase()]} />
-				<span>{coin}</span>
-			</Button>
+  return (
+    <div className="flex flex-col gap-1 items-start">
+      <Button variant="ghost" size="sm" className="p-1">
+        <img className="mr-2 w-6" src={COIN_SRC[coin.toLowerCase()]} />
+        <span>{coin}</span>
+      </Button>
 
-			<div className="inline-block">
-				<Button variant="ghost" size="sm" className="p-1">
-					<ReactSVG
-						src={SVG_SRC[cex.toLowerCase()]}
-						className="mr-2 w-6"
-					/>
-					<span>{cex}</span>
-				</Button>
+      <div className="inline-block">
+        <Button variant="ghost" size="sm" className="p-1">
+          <ReactSVG
+            src={getQiniuSrc(cex.toLowerCase(), "svg")}
+            className="mr-2 w-6"
+          />
+          <span>{cex}</span>
+        </Button>
 
-				{dex ? (
-					<Button variant="ghost" size="sm" className="p-1">
-						<ReactSVG
-							src={SVG_SRC[dex.toLowerCase()]}
-							className="mr-2 w-6"
-						/>
-						<span>{dex}</span>
-					</Button>
-				) : undefined}
-			</div>
-		</div>
-	);
+        {dex ? (
+          <Button variant="ghost" size="sm" className="p-1">
+            <ReactSVG
+              src={getQiniuSrc(dex.toLowerCase(), "svg")}
+              className="mr-2 w-6"
+            />
+            <span>{dex}</span>
+          </Button>
+        ) : undefined}
+      </div>
+    </div>
+  );
 }
